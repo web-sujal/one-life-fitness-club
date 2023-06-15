b698ccfbef2ed960af4ab33453092e5fadbd3f6e
web-sujal <web.sujal@gmail.com>
2023-06-15 15:52:21 +0530

    Added animations to Home Section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,12 +11,17 @@ const Home = ({ setSelectedPage }: Props) => {
     <section id="home">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
-        className="min-h-screen bg-lightCream pt-28"
+        className="min-h-screen bg-lightCream pt-40 md:pt-0"
       >
         {/* IMAGE AND HEADINGS */}
-        <div className="mx-auto flex w-[80vw] basis-1/2 flex-col items-center justify-center pt-12 md:flex-row md:gap-6 md:pt-0">
+        <div className="mx-auto flex h-screen w-[80vw] basis-1/2 flex-col items-center justify-center pt-20 md:flex-row md:gap-6">
           {/* HEADING */}
-          <div className="flex flex-col items-center justify-center gap-2 text-center font-heading text-5xl font-extrabold text-primary-500 md:w-1/2 md:max-w-lg md:items-start md:pt-0 md:text-left md:text-6xl">
+          <motion.div
+            initial={{ x: "-50px", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col items-center justify-center gap-2 text-center font-heading text-5xl font-extrabold text-primary-500 md:w-1/2 md:max-w-lg md:items-start md:pt-0 md:text-left md:text-6xl"
+          >
             <h2>
               Transform Your <span className="text-secondary-300">Body</span>,
               Transform Your <span className="text-secondary-300">Life</span>!
@@ -34,17 +39,22 @@ const Home = ({ setSelectedPage }: Props) => {
             <div className="text-lg">
               <ActionButton>Join Now</ActionButton>
             </div>
-          </div>
+          </motion.div>
 
           {/* IMAGE */}
 
-          <div className="basis-1/2 md:w-1/2">
+          <motion.div
+            initial={{ x: "50px", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="basis-1/2 md:w-1/2"
+          >
             <img
               src="/images/ronit.png"
               className=" object-cover"
               alt="trainer-img"
             />
-          </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
@@ -52,6 +62,3 @@ const Home = ({ setSelectedPage }: Props) => {
 };
 
 export default Home;
-
-// Headline: "Transform Your Body, Transform Your Life!"
-// Subheadline: "Unleash Your Full Potential at One Life Fitness Club"
